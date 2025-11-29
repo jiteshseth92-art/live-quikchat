7e2bfc280c0574b82b7713b003417d53a5052f02
jiteshseth92-art <jiteshseth92@gmail.com>
2025-11-30 04:25:45 +0530

    server.js

--- server.js
+++ server.js
@@ -1,44 +1,41 @@
 const express = require("express");
 const app = express();
-const http = require("http").createServer(app);
+const https = require("https");
 const { Server } = require("socket.io");
-const io = new Server(http, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
-});
-const path = require("path");
+const cors = require("cors");
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
+app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+const server = https.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
 });
+
+let waitingUser = null;
 
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
-  socket.on("find", () => {
-    socket.broadcast.emit("found", socket.id);
-  });
+  if (!waitingUser) {
+    waitingUser = socket;
+    socket.emit("waiting");
+  } else {
+    waitingUser.emit("partner", socket.id);
+    socket.emit("partner", waitingUser.id);
+    waitingUser = null;
+  }
 
-  socket.on("offer", (data) => {
-    socket.to(data.to).emit("offer", { sdp: data.sdp, from: socket.id });
-  });
-
-  socket.on("answer", (data) => {
-    socket.to(data.to).emit("answer", { sdp: data.sdp, from: socket.id });
-  });
-
-  socket.on("ice", (data) => {
-    socket.to(data.to).emit("ice", data.ice);
-  });
+  socket.on("offer", (data) => socket.to(data.to).emit("offer", data));
+  socket.on("answer", (data) => socket.to(data.to).emit("answer", data));
+  socket.on("ice", (data) => socket.to(data.to).emit("ice", data));
 
   socket.on("disconnect", () => {
     console.log("User disconnected:", socket.id);
+    if (waitingUser && waitingUser.id === socket.id) waitingUser = null;
   });
 });
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(10000, () => {
+  console.log("Server running on port 10000");
+});
