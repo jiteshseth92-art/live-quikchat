9ec19bb78931e5511e62d921f1bc77cd842c94f2
jiteshseth92-art <jiteshseth92@gmail.com>
2025-12-01 00:53:38 +0530

    server.js

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
+
 const io = new Server(server, {
-    cors: { origin: "*" }
+  cors: { origin: "*" }
+});
+
+// Serve frontend
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 let waitingUser = null;
@@ -25,20 +34,27 @@ io.on("connection", (socket) => {
     }
   });
 
-  socket.on("send-message", (msg) => {
-    if (socket.partner) {
-      io.to(socket.partner).emit("receive-message", msg);
-    }
+  socket.on("offer", (data) => {
+    io.to(data.to).emit("offer", { sdp: data.sdp, from: socket.id });
+  });
+
+  socket.on("answer", (data) => {
+    io.to(data.to).emit("answer", { sdp: data.sdp, from: socket.id });
+  });
+
+  socket.on("ice-candidate", (data) => {
+    io.to(data.to).emit("ice-candidate", data.candidate);
   });
 
   socket.on("disconnect", () => {
+    if (waitingUser === socket.id) waitingUser = null;
+
     if (socket.partner) {
       io.to(socket.partner).emit("partner-left");
     }
-    if (waitingUser === socket.id) {
-      waitingUser = null;
-    }
   });
+
 });
 
-server.listen(3000, () => console.log("Server running on 3000"));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log("Server Running on port " + PORT));
