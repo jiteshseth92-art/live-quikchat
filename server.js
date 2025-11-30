1f975218f8e41d14d913b4e729aeb8aa17510d80
jiteshseth92-art <jiteshseth92@gmail.com>
2025-11-30 09:28:55 +0530

    server.js

--- server.js
+++ server.js
@@ -1,51 +1,49 @@
-const express = require("express");
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import path from "path";
+import cors from "cors";
+
 const app = express();
-const http = require("http");
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const cors = require("cors");
-
 app.use(cors());
-app.use(express.static("public")); // index.html yahi folder me hoga
+app.use(express.static(path.join(process.cwd(), "public")));
 
+const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 });
-
-let waitingUsers = [];
 
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
-  socket.on("find_partner", () => {
-    if (waitingUsers.length > 0) {
-      const partner = waitingUsers.pop();
-
-      socket.emit("partner_found", partner);
-      io.to(partner).emit("partner_found", socket.id);
-    } else {
-      waitingUsers.push(socket.id);
-    }
+  socket.on("find-partner", () => {
+    socket.broadcast.emit("partner-found", socket.id);
   });
 
-  socket.on("signal", (data) => {
-    io.to(data.to).emit("signal", {
-      from: socket.id,
-      sdp: data.sdp
-    });
+  socket.on("offer", (data) => {
+    io.to(data.to).emit("offer", data);
+  });
+
+  socket.on("answer", (data) => {
+    io.to(data.to).emit("answer", data);
+  });
+
+  socket.on("candidate", (data) => {
+    io.to(data.to).emit("candidate", data);
   });
 
   socket.on("disconnect", () => {
-    waitingUsers = waitingUsers.filter((id) => id !== socket.id);
-    console.log("Disconnected:", socket.id);
+    console.log("User disconnected:", socket.id);
+    socket.broadcast.emit("partner-left");
   });
 });
 
-// Render PORT fix
-const PORT = process.env.PORT || 10000;
-server.listen(PORT, () => {
-  console.log("Server running on port:", PORT);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "public/index.html"));
 });
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`Server running on port ${port}`));
