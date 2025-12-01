180942e9a9f0306feefbaceafe1329bea46bec91
jiteshseth92-art <jiteshseth92@gmail.com>
2025-12-01 12:49:51 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -1,60 +1,48 @@
 const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const path = require("path");
-
 const app = express();
-const server = http.createServer(app);
-
-const io = new Server(server, {
-  cors: { origin: "*" }
-});
-
-// Serve frontend
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-let waitingUser = null;
-
-io.on("connection", (socket) => {
-
-  socket.on("find-partner", () => {
-    if (waitingUser && waitingUser !== socket.id) {
-      const partner = waitingUser;
-      waitingUser = null;
-
-      socket.partner = partner;
-      io.to(partner).emit("partner-found", socket.id);
-      socket.emit("partner-found", partner);
-    } else {
-      waitingUser = socket.id;
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
     }
-  });
-
-  socket.on("offer", (data) => {
-    io.to(data.to).emit("offer", { sdp: data.sdp, from: socket.id });
-  });
-
-  socket.on("answer", (data) => {
-    io.to(data.to).emit("answer", { sdp: data.sdp, from: socket.id });
-  });
-
-  socket.on("ice-candidate", (data) => {
-    io.to(data.to).emit("ice-candidate", data.candidate);
-  });
-
-  socket.on("disconnect", () => {
-    if (waitingUser === socket.id) waitingUser = null;
-
-    if (socket.partner) {
-      io.to(socket.partner).emit("partner-left");
-    }
-  });
-
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log("Server Running on port " + PORT));
+
+// Serve static public folder
+const path = require("path");
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+// WebRTC
+io.on("connection", (socket) => {
+    console.log("User connected", socket.id);
+
+    socket.on("find", () => {
+        socket.broadcast.emit("match", socket.id);
+    });
+
+    socket.on("offer", (data) => {
+        socket.to(data.to).emit("offer", data);
+    });
+
+    socket.on("answer", (data) => {
+        socket.to(data.to).emit("answer", data);
+    });
+
+    socket.on("ice", (data) => {
+        socket.to(data.to).emit("ice", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected", socket.id);
+    });
+});
+
+http.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
