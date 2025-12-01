2d4cdfff251ee8bdbf4aba0a9dbdbf595daa94d8
jiteshseth92-art <jiteshseth92@gmail.com>
2025-12-02 04:08:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,37 +1,47 @@
 import express from "express";
+import { Server } from "socket.io";
+import http from "http";
 import cors from "cors";
 import { AccessToken } from "livekit-server-sdk";
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.static("public"));
 
-const LIVEKIT_URL = "wss://quikchat-z062h7l1.livekit.cloud";
-const LIVEKIT_API_KEY = "APIUxHVxWWDYkk7";
-const LIVEKIT_API_SECRET = "WtWnIzlW7HxbMTfkswbxSvLDimwtgRDPf2ZEI08HOJb";
-
-// generate token route
-app.post("/getToken", (req, res) => {
-  const { identity, roomName } = req.body;
-
-  const at = new AccessToken(LIVEKIT_API_KEY, LIVEKIT_API_SECRET, {
-    identity,
-  });
-
-  at.addGrant({
-    roomJoin: true,
-    canPublish: true,
-    canSubscribe: true,
-    room: roomName,
-  });
-
-  const token = at.toJwt();
-  res.json({ token });
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
 });
 
-app.listen(4000, () => console.log("Token server started on port 4000"));
-app.use(express.static("public"));
+let waitingUser = null;
+
+io.on("connection", (socket) => {
+  console.log("User Joined:", socket.id);
+
+  socket.on("findPartner", () => {
+    if (waitingUser && waitingUser !== socket.id) {
+      io.to(waitingUser).emit("partnerFound", socket.id);
+      io.to(socket.id).emit("partnerFound", waitingUser);
+      waitingUser = null;
+    } else {
+      waitingUser = socket.id;
+    }
+  });
+
+  socket.on("signal", (data) => {
+    io.to(data.to).emit("signal", {
+      from: socket.id,
+      signal: data.signal,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    if (waitingUser === socket.id) waitingUser = null;
+  });
+});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
+
+server.listen(4000, () => console.log("Server Running 4000"));
