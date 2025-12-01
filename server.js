a8babe1903424518b8b674169352d08a1847d3ed
jiteshseth92-art <jiteshseth92@gmail.com>
2025-12-01 14:48:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -238,3 +238,5 @@ function sanitizeProfile(u) {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, "0.0.0.0", () => console.log("Server running on port", PORT));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, "0.0.0.0", () => console.log("Server started on port " + PORT));
