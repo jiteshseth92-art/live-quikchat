a61312376ea07c738bf012b0f022cdfcad7087b9
jiteshseth92-art <jiteshseth92@gmail.com>
2025-12-01 13:36:57 +0530

    server.js

--- server.js
+++ server.js
@@ -2,41 +2,45 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
-});
-
-let waitingUser = null;
-
-io.on("connection", (socket) => {
-    console.log("User connected:", socket.id);
-
-    socket.on("find", () => {
-        if (waitingUser && waitingUser !== socket.id) {
-            io.to(socket.id).emit("match", waitingUser);
-            io.to(waitingUser).emit("match", socket.id);
-            waitingUser = null;
-        } else {
-            waitingUser = socket.id;
-        }
-    });
-
-    socket.on("signal", (data) => {
-        io.to(data.to).emit("signal", {
-            from: socket.id,
-            signal: data.signal,
-        });
-    });
-
-    socket.on("disconnect", () => {
-        console.log("Disconnected:", socket.id);
-        if (waitingUser === socket.id) waitingUser = null;
-    });
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
 });
 
 app.use(express.static("public"));
 
+let waitingUser = null;
+
+io.on("connection", (socket) => {
+  console.log("User connected: " + socket.id);
+
+  socket.on("findPartner", () => {
+    if (waitingUser) {
+      socket.partner = waitingUser;
+      waitingUser.partner = socket;
+
+      socket.emit("partnerFound", waitingUser.id);
+      waitingUser.emit("partnerFound", socket.id);
+
+      waitingUser = null;
+    } else {
+      waitingUser = socket;
+      socket.emit("waiting");
+    }
+  });
+
+  socket.on("signal", (data) => {
+    if (socket.partner) {
+      socket.partner.emit("signal", { signal: data.signal, from: socket.id });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    if (waitingUser === socket) waitingUser = null;
+    if (socket.partner) socket.partner.emit("partnerDisconnected");
+  });
+});
+
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log("Server running on", PORT));
+http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
