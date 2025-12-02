d6d04aee5b8e0e55ed5efa0ec9861c588a274934
jiteshseth92-art <jiteshseth92@gmail.com>
2025-12-02 10:36:41 +0530

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -1,6 +1,11 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const server = http.createServer(app);
@@ -11,14 +16,14 @@ const io = new Server(server, {
   }
 });
 
+app.use(express.static(path.join(__dirname, "public")));
+
 let waitingUser = null;
 
 io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
+  console.log("User Joined:", socket.id);
 
   socket.on("find", () => {
-    console.log("Find request by:", socket.id);
-
     if (waitingUser && waitingUser !== socket.id) {
       io.to(socket.id).emit("match", waitingUser);
       io.to(waitingUser).emit("match", socket.id);
@@ -30,14 +35,24 @@ io.on("connection", (socket) => {
   });
 
   socket.on("offer", (data) => {
-    io.to(data.target).emit("offer", {
-      sdp: data.sdp,
-      caller: socket.id
-    });
+    io.to(data.target).emit("offer", { sdp: data.sdp, caller: socket.id });
   });
 
   socket.on("answer", (data) => {
-    io.to(data.target).emit("answer", {
-      sdp: data.sdp
-    });
+    io.to(data.target).emit("answer", { sdp: data.sdp });
   });
+
+  socket.on("ice", (data) => {
+    io.to(data.target).emit("ice", { candidate: data.candidate });
+  });
+
+  socket.on("disconnect", () => {
+    if (waitingUser === socket.id) waitingUser = null;
+    console.log("User Left:", socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 10000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
